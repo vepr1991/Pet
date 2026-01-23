db2586d588bfd7507132f2681e6fa18152b707b2
vepr1991 <vepr1991@gmail.com>
2026-01-23 22:34:54 +0500

    обновил index.html

--- keyboards.py
+++ keyboards.py
@@ -3,21 +3,15 @@ import database as db
 
 
 def get_main_kb(user_id, admin_id):
-    # Основные URL (замените на свои актуальные ссылки на GitHub Pages)
-    # На client.html мы как раз и сделаем ваш выбор "Мастер/Клиент"
     app_url = "https://vepr1991.github.io/Pet/index.html"
-
-    # 1. Проверяем: является ли пользователь Главным Админом или Мастером в базе
     is_user_master = (user_id == admin_id) or db.is_master(user_id)
 
     if is_user_master:
-        # Клавиатура для МАСТЕРА
         buttons = [
             [KeyboardButton(text="⚙️ Панель управления", web_app=WebAppInfo(url=app_url))],
-            [KeyboardButton(text="📋 Посмотреть записи (текст)")]
+            [KeyboardButton(text="📊 Посмотреть записи (Админ)")] # Текст должен быть 1 в 1 как в admin.py
         ]
     else:
-        # Клавиатура для КЛИЕНТА
         buttons = [
             [KeyboardButton(text="Записаться на груминг ✂️", web_app=WebAppInfo(url=app_url))]
         ]
