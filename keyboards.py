45b857672ce808e582e4e8bb3bc0a3fedcc71a99
vepr1991 <vepr1991@gmail.com>
2026-01-24 14:05:26 +0500

    обновил admin.html

--- keyboards.py
+++ keyboards.py
@@ -1,26 +1,29 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
 import database as db
 
+
 def get_main_kb(user_id, admin_id):
     base_url = "https://vepr1991.github.io/Pet"
-    is_master = db.is_master(user_id) # Проверка в таблице masters
+    is_master = db.is_master(user_id)
     is_admin = (user_id == admin_id)
 
     buttons = []
 
-    # 1. ШАГ: ВЫ (ГЛОБАЛЬНЫЙ АДМИН)
     if is_admin:
-        # Убираем "Тест записи", оставляем только управление и проверку
+        # Добавляем ID в ссылку для админа
+        buttons.append(
+            [KeyboardButton(text="⚙️ Админ-панель", web_app=WebAppInfo(url=f"{base_url}/admin.html?master={user_id}"))])
         buttons.append([KeyboardButton(text="📊 Посмотреть записи (Все)")])
-        buttons.append([KeyboardButton(text="⚙️ Админ-панель", web_app=WebAppInfo(url=f"{base_url}/admin.html"))])
         buttons.append([KeyboardButton(text="🔗 Моя ссылка")])
 
-    # 3. ШАГ: ЗАРЕГИСТРИРОВАННЫЙ МАСТЕР (уже есть в базе)
     elif is_master:
-        buttons.append([KeyboardButton(text="⚙️ Панель мастера", web_app=WebAppInfo(url=f"{base_url}/admin.html"))])
+        # Добавляем ID в ссылку для мастера
+        buttons.append([KeyboardButton(text="⚙️ Панель мастера",
+                                       web_app=WebAppInfo(url=f"{base_url}/admin.html?master={user_id}"))])
         buttons.append([
             KeyboardButton(text="🔗 Моя ссылка"),
-            KeyboardButton(text="✂️ Редактировать услуги", web_app=WebAppInfo(url=f"{base_url}/admin.html#services"))
+            KeyboardButton(text="✂️ Редактировать услуги",
+                           web_app=WebAppInfo(url=f"{base_url}/admin.html?master={user_id}#services"))
         ])
 
     # 2. ШАГ: МАСТЕР (НЕТ В БАЗЕ)
